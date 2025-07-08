19beed1f27c8a9d467e0ee980e475f1e57a25824
nexbuf <cekopar2@gmail.com>
2025-07-08 09:33:44 +0300

    Update CGPAcalc.cpp

--- c++/CGPAcalc/CGPAcalc.cpp
+++ c++/CGPAcalc/CGPAcalc.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+int main() {
+    std::vector<double> credits;
+    int n = 0;
+    double credit,grade,sum1 = 0;
+    std::cout << "Enter number of courses: ";
+    std::cin >> n;
+    for ( int i = 0; i < n; i++) {
+        std::cout << "Please Enter Credit and Grade per course (Credit 1st then Grade and add a space between each number): ";
+        std::cin >> credit;
+        credits.push_back(credit);
+        std::cin >> grade;
+        sum1 += grade*credit;
+    }
+    double sum2 = accumulate(credits.begin(),credits.end(),0.0);
+    double cgpa = sum1/sum2;
+    std::cout << "CGPA is: " << cgpa;
+    return 0;
+}
 
